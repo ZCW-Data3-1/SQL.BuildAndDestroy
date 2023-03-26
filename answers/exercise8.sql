1d697464827a3380b9964724f5f23e03910e15e3
dhannyamohangeetha <dhannyabrijesh.ktm@gmail.com>
2023-03-26 14:07:14 -0400

    Added sql scripts

---
+++ answers/exercise8.sql
@@ -0,0 +1,10 @@
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES ('Jane Doe', '57 Union St', 'Glasgow', 6789, 'Scotland');
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES ('Aravind Babu', '34 ff St', 'Newyork', 5555, 'USA');
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES ('Matt Hoke', '444 ssf St', 'Berlin', 3456, 'Germany');
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES ('ssss ddd', '44 ffg St', 'Philadelphia', 4333, 'USA');
+INSERT INTO Students (StudentName, Address, City, PostalCode, Country) VALUES ('we rr', '55 ff St', 'Trenton', 7677, 'USA');
+INSERT INTO Students (StudentName, Address, City, Country) VALUES ('fffdfdf', '54 jhjj St', 'Philadelphia','USA');
+INSERT INTO Students (StudentName, Address, City, Country) VALUES ('fdf', '22 bbvb St', 'Trenton', 'USA');
+
+
+
